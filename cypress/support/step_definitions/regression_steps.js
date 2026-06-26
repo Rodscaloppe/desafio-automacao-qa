41e27c8f3f09df300f6256b51788357271a74e54
rodscaloppe <roscaloppe@gmail.com>
2026-06-26 17:06:30 -0300

    melhorias: duplicacao cadastro LOgin page

--- cypress/support/step_definitions/regression_steps.js
+++ cypress/support/step_definitions/regression_steps.js
@@ -23,21 +23,13 @@ Given('crio uma conta nova com nome {string}, email randômico e dados completos
   const email = `qa_${Date.now()}_${faker.internet.email()}`;
   
   LoginPage.submitSignupStep1(registeredName, email);
-  
-  // Preencher formulário restante de cadastro (mesma lógica do LoginPage.js)
-  cy.get('[data-qa="password"]').type('Password123!');
-  cy.get('[data-qa="first_name"]').type(registeredName);
-  cy.get('[data-qa="last_name"]').type(faker.name.lastName());
-  cy.get('[data-qa="address"]').type('123 QA St');
-  cy.get('[data-qa="country"]').select('United States');
-  cy.get('[data-qa="state"]').type('São Paulo');
-  cy.get('[data-qa="city"]').type('Campinas');
-  cy.get('[data-qa="zipcode"]').type('13000');
-  cy.get('[data-qa="mobile_number"]').type('19999999999');
-  
-  cy.get('[data-qa="create-account"]').click();
-  cy.get('[data-qa="account-created"]').should('be.visible');
-  cy.get('[data-qa="continue-button"]').click();
+  LoginPage.completeSignupForm(registeredName, {
+    address: '123 QA St',
+    state: 'São Paulo',
+    city: 'Campinas',
+    zipcode: '13000',
+    mobileNumber: '19999999999'
+  });
   
   LoginPage.verifyLoggedIn();
 });
